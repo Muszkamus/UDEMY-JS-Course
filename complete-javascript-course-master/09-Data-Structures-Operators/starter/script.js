88abbd65d4905edef5f6dd5f7d0e0300ab264dbb
Muszkamus <balicki56@gmail.com>
2024-11-18 23:40:50 +0000

    Working with arrays

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -47,3 +47,40 @@ const restaurant = {
     console.log(mainIngredient, otherIngredients);
   },
 };
+
+const airLine = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]); // A
+console.log(plane[1]); // 3
+console.log(plane[2]); // 2
+
+console.log(airLine.length); // 16
+console.log('B737'.length); // 4
+
+console.log(airLine.indexOf('r')); // 6 as this is thep osition of the character (Even the space counts)
+console.log(airLine.lastIndexOf('r')); // space 10 but tfrom the right to left
+console.log(airLine.indexOf('Portugal')); // -1 if not found, and 8 if found (position of the first letter)
+
+console.log(airLine.slice(4)); // Slices first 4 characters
+console.log(airLine.slice(4, 7)); // Slices first 4 characters and starts again at 7 (7-4) = 4 characters
+
+console.log(airLine.slice(0, airLine.indexOf(' '))); // Prints first word
+console.log(airLine.slice(airLine.lastIndexOf(' ') + 1)); //Prints last word and add +1 to get rid of the space at the beginning
+
+console.log(airLine.slice(-2)); // Shows the last 2 characters
+console.log(airLine.slice(1, -1)); //Cuts first and last character of the string
+console.log('-------------------------');
+
+const checkMiddleSeat = function (seat) {
+  // B and E and middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') {
+    console.log("It's a middle seat!");
+  } else console.log('');
+};
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(new String('radek')); //It's an object
