9db2e89bcebe1abd37abec866960026c2e95b220
Muszkamus <balicki56@gmail.com>
2024-12-18 15:19:31 +0000

    Update script.js

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,8 +1,12 @@
 'use strict';
 
-function makeUpperCase(str) {
-  const upperCase = str.toUpperCase();
-  return upperCase;
+function evenOrOdd(number) {
+  for (let i = 0; i < number.length; i++)
+    if (number[i] % 2 === 0) {
+      console.log('lol');
+    } else {
+      console.log(i);
+    }
 }
 
-console.log(makeUpperCase('we are the champions'));
+evenOrOdd(2, 5, 1, 6, 7, 8, 9);
