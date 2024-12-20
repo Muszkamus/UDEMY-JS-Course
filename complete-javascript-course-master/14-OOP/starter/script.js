64b59d393b3baaece9a23c50dc8f4dd6a03dde63
Muszkamus <balicki56@gmail.com>
2024-12-20 20:57:04 +0000

    callenges

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,11 +1,1 @@
 'use strict';
-
-function highAndLow(numbers) {
-  const numArray = numbers.split(' ').map(Number);
-  const max = Math.max(...numArray);
-  const min = Math.min(...numArray);
-  return `${max} ${min}`;
-}
-
-// Test cases
-console.log(highAndLow('1 2 3 4 5')); // Output: "5 1"
